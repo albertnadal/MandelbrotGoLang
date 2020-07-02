d8eca38f5fa1e9490bce0a38c7cd6e00b4c7ef6b
Albert Nadal Garriga <anadalg@gmail.com>
2020-07-02 17:34:54 +0200

    Use Hsv color palette

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module mandelbrot-fractal
 
 go 1.13
 
-require github.com/gen2brain/raylib-go v0.0.0-20200625212157-7bdb60d758ed
+require (
+	github.com/gen2brain/raylib-go v0.0.0-20200625212157-7bdb60d758ed
+	github.com/lucasb-eyer/go-colorful v1.0.3
+)
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gen2brain/raylib-go/raygui"
 	"github.com/gen2brain/raylib-go/raylib"
+	"github.com/lucasb-eyer/go-colorful"
 	"math"
 	"runtime"
 	"sync"
@@ -11,13 +12,15 @@ import (
 )
 
 const MAX_THREADS int32 = 16
+const SCREEN_WIDTH int32 = 1280
+const SCREEN_HEIGHT int32 = 720
 
 type Mandelbrot struct {
 	ScreenWidth         int32
 	ScreenHeight        int32
 	Pixels              [][]rl.Color
 	MagnificationFactor float64
-	MaxIterations       uint32
+	MaxIterations       float64
 	PanX                float64
 	PanY                float64
 	WaitGroup           sync.WaitGroup
@@ -26,6 +29,7 @@ type Mandelbrot struct {
 	ThreadsProcessTimes []time.Duration
 	TotalProcessTime    time.Duration
 	ZoomLevel           float64
+	Canvas              rl.RenderTexture2D
 }
 
 func main() {
@@ -37,11 +41,11 @@ func main() {
 	// Set-up the Go runtime to use all the available CPU cores
 	runtime.GOMAXPROCS(totalCores)
 
+	rl.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Mandelbrot fractal")
+	rl.SetTargetFPS(30)
+
 	fractal := Mandelbrot{}
 	fractal.Init()
-
-	rl.InitWindow(fractal.ScreenWidth, fractal.ScreenHeight, "Mandelbrot fractal")
-	rl.SetTargetFPS(30)
 
 	for !rl.WindowShouldClose() {
 		fractal.Update()
@@ -49,6 +53,7 @@ func main() {
 		fractal.ProcessKeyboard()
 	}
 
+	rl.UnloadTexture(fractal.Canvas.Texture)
 	rl.CloseWindow()
 }
 
@@ -63,7 +68,7 @@ func (m *Mandelbrot) Update() {
 	start := time.Now()
 	for i := int32(0); i < m.MaxThreads; i++ {
 		m.WaitGroup.Add(1)
-		go m.UpdateArea(i, i*areaWidth, 0, i*areaWidth+areaWidth, areaHeight)
+		go m.UpdateArea(i, i*areaWidth + 1, 0, i*areaWidth + areaWidth + i, areaHeight)
 	}
 	m.WaitGroup.Wait()
 	m.TotalProcessTime = time.Since(start)
@@ -73,8 +78,8 @@ func (m *Mandelbrot) UpdateArea(thread_index int32, x_start int32, y_start int32
 	defer m.WaitGroup.Done()
 
 	start := time.Now()
-	for x := int32(x_start); x <= x_end; x++ {
-		for y := int32(y_start); y <= y_end; y++ {
+	for x := x_start; (x <= x_end) && (x < m.ScreenWidth); x++ {
+		for y := y_start; y <= y_end; y++ {
 			m.Pixels[x][y] = m.getPixelColorAtPosition((float64(x)/m.MagnificationFactor)-m.PanX, (float64(y)/m.MagnificationFactor)-m.PanY)
 		}
 	}
@@ -87,15 +92,19 @@ func (m *Mandelbrot) getPixelColorAtPosition(x float64, y float64) rl.Color {
 	imaginaryComponent := y
 	tempRealComponent := float64(0.0)
 	tempImaginaryComponent := float64(0.0)
+	colorHSV := colorful.Color{}
+	colorRGB := rl.Color{}
 
-	for i := uint32(0); i < m.MaxIterations; i++ {
+	for i := float64(0); i < m.MaxIterations; i++ {
 		tempRealComponent = (realComponent * realComponent) - (imaginaryComponent * imaginaryComponent) + x
 		tempImaginaryComponent = 2.0*realComponent*imaginaryComponent + y
 		realComponent = tempRealComponent
 		imaginaryComponent = tempImaginaryComponent
 
 		if realComponent*imaginaryComponent > 5 {
-			return rl.NewColor(uint8((i*255)/m.MaxIterations), 0, 0, 255)
+			colorHSV = colorful.Hsv(i*360/m.MaxIterations, 0.98, 0.922) // hue bar color (Hsv)
+			colorRGB = rl.NewColor(uint8(colorHSV.R*255), uint8(colorHSV.G*255), uint8(colorHSV.B*255), 255)
+			return colorRGB
 		}
 	}
 
@@ -105,11 +114,14 @@ func (m *Mandelbrot) getPixelColorAtPosition(x float64, y float64) rl.Color {
 func (m *Mandelbrot) Draw() {
 	rl.BeginDrawing()
 	rl.ClearBackground(rl.Black)
+	rl.BeginTextureMode(m.Canvas);
 	for x := int32(0); x < m.ScreenWidth; x++ {
 		for y := int32(0); y < m.ScreenHeight; y++ {
 			rl.DrawPixel(x, y, m.Pixels[x][y])
 		}
 	}
+	rl.EndTextureMode()
+	rl.DrawTexture(m.Canvas.Texture, 0,0, rl.RayWhite)
 
 	raygui.SetStyleProperty(raygui.GlobalTextFontsize, 20.0)
 	raygui.SetStyleProperty(raygui.LabelTextColor, 16448250)
@@ -127,51 +139,54 @@ func (m *Mandelbrot) Draw() {
 func (m *Mandelbrot) ProcessKeyboard() {
 	m.NeedUpdate = false
 	if rl.IsKeyDown(rl.KeyLeft) {
-		m.PanX -= 0.5 / math.Exp2(m.ZoomLevel/1.65)
+		m.PanX -= 0.5/*0.00001*/ / math.Exp2(m.ZoomLevel/1.65)
 		m.NeedUpdate = true
 	}
 
 	if rl.IsKeyDown(rl.KeyRight) {
-		m.PanX += 0.5 / math.Exp2(m.ZoomLevel/1.65)
+		m.PanX += 0.5/*0.00001*/ / math.Exp2(m.ZoomLevel/1.65)
 		m.NeedUpdate = true
 	}
 
 	if rl.IsKeyDown(rl.KeyUp) {
-		m.PanY -= 1.0 / math.Exp2(m.ZoomLevel/1.65)
+		m.PanY += 0.5/*0.00001*/ / math.Exp2(m.ZoomLevel/1.65)
 		m.NeedUpdate = true
 	}
 
 	if rl.IsKeyDown(rl.KeyDown) {
-		m.PanY += 1.0 / math.Exp2(m.ZoomLevel/1.65)
+		m.PanY -= 0.5/*0.00001*/ / math.Exp2(m.ZoomLevel/1.65)
 		m.NeedUpdate = true
 	}
 
 	if rl.IsKeyDown(rl.KeyA) {
 		m.ZoomLevel += 0.1
 		m.MagnificationFactor = 400 + math.Exp2(m.ZoomLevel)
-    m.MaxIterations = 50 + uint32(15.5*m.ZoomLevel)
+    m.MaxIterations = 100//12000//6000 + uint32(15.5*m.ZoomLevel)
 		m.NeedUpdate = true
+		fmt.Printf("ZoomLevel. (%f) MaxIterations: (%d) PanX: (%3.40f) PanY: (%3.40f)\n", m.ZoomLevel, m.MaxIterations, m.PanX, m.PanY)
 	}
 
 	if rl.IsKeyDown(rl.KeyS) {
 		m.ZoomLevel -= 0.1
 		m.MagnificationFactor = 400 + math.Exp2(m.ZoomLevel)
-    m.MaxIterations = 50 + uint32(15.5*m.ZoomLevel)
+    m.MaxIterations = 100//12000//6000 + uint32(15.5*m.ZoomLevel)
 		m.NeedUpdate = true
+		fmt.Printf("ZoomLevel. (%f) MaxIterations: (%f) PanX: (%3.40f) PanY: (%3.40f)\n", m.ZoomLevel, m.MaxIterations, m.PanX, m.PanY)
 	}
 }
 
 func (m *Mandelbrot) Init() {
-	m.ScreenWidth = 1280
-	m.ScreenHeight = 720
+	m.ScreenWidth = SCREEN_WIDTH*2
+	m.ScreenHeight = SCREEN_HEIGHT*2
+	m.ZoomLevel = 1//52.9 //1
 	m.MagnificationFactor = 400.0
-	m.MaxIterations = 50
-	m.PanX = 2.0
-	m.PanY = 1.0
+	m.MaxIterations = 8192//50
+	m.PanX = 0.1367469998666127062314501472428673878312 //2.0
+	m.PanY = 0.6500099999191820687727272343181539326906 //1.0
 	m.NeedUpdate = true
-	m.ZoomLevel = 1
 	m.MaxThreads = MAX_THREADS
 	m.ThreadsProcessTimes = make([]time.Duration, m.MaxThreads)
+	m.Canvas = rl.LoadRenderTexture(m.ScreenWidth, m.ScreenHeight)
 
 	m.Pixels = make([][]rl.Color, m.ScreenWidth)
 	for i := int32(0); i < m.ScreenWidth; i++ {
@@ -180,7 +195,7 @@ func (m *Mandelbrot) Init() {
 
 	for x := int32(0); x < m.ScreenWidth; x++ {
 		for y := int32(0); y < m.ScreenHeight; y++ {
-			m.Pixels[x][y] = rl.NewColor(255, 0, 0, 255)
+			m.Pixels[x][y] = rl.NewColor(0, 0, 0, 255)
 		}
 	}
 }
